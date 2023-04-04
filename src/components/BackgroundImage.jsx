554ab58c837a552a9b2ced0f292b3b7c216c5300
tqnaam <18770@student.vgu.edu.vn>
2023-04-04 13:55:57 +0700

    add background img and assets

--- src/components/BackgroundImage.jsx
+++ src/components/BackgroundImage.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
-
+import background from "../assets/login.jpg";
+import styled from "styled-components";
+import Container from "@material-ui/core/Container";
 export default function BackgroundImage() {
   return (
-    <div>BackgroundImage</div>
+    <Container>
+        <img src="{ background }" alt="background" />
+    </Container>
   )
 }
+
